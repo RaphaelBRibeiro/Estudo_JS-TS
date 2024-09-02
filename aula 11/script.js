e1f99635eec53e68ff89d989fd1fdbf39666e6b4
Raphael Braga Ribeiro <93213732+RaphaelBRibeiro@users.noreply.github.com>
2024-09-02 17:20:28 -0300

    Add files via upload

---
+++ aula 11/script.js
@@ -0,0 +1,20 @@
+// receber 2 numeros trazer o resultado e dps infomar o valor pelo alert
+
+let num1 = prompt ('Digite um Numero: ');
+let num2 = prompt ('Digite um numero: ');
+
+num1 = Number (num1);
+num2 = Number (num2);
+
+const resultado = num1 + num2;
+
+window.alert (`O resultado da soma foi:  ${resultado} `);
+
+/* ou 
+let num1 = prompt ('Digite um Numero: ');
+let num2 = prompt ('Digite um numero: ');
+
+num1 = Number (num1);
+num2 = Number (num2);
+
+window.alert (`O resultado da soma foi:  ${num1 + num2} `);  */
